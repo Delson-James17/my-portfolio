1028e5018c0c9a61884464039a3ca2a08288edfa
Delson-James17 <delsonjames17@gmail.com>
2025-02-09 16:51:24 +0800

    fix vite.svg

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // Change this to your GitHub repo name
+  base: '/my-portfolioV2/', // Change this to your GitHub repo name
 });
