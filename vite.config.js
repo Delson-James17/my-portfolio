c5264ba8481b55b9aa1c6b5e7e303a5e6b067813
Delson-James17 <delsonjames17@gmail.com>
2025-03-12 14:56:35 +0800

    AddFavicon and Fix the menus

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolioV2/', // Change this to your GitHub repo name
-});
+  base: '/my-portfolio/', 
+  });
+
