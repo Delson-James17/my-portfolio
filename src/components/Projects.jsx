db5b6cf1334e38f9735a637be8c862f08a27fd7e
Delson-James17 <delsonjames17@gmail.com>
2025-03-14 19:06:47 +0800

    Add App Projectss

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,17 @@
 import { Container } from "react-bootstrap";
 import { FaExternalLinkAlt } from "react-icons/fa";
-import Project4 from '../assets/Project4v1.png'
+import Project3 from '../assets/Project4v1.png'
 import Project1 from '../assets/ChatApp.jpg'
+import Project2 from '../assets/ValentineApp.png'
 import "../css/Projects.css";
 
 const projects = [
   { id: 1, image: Project1, title: "ChatApp", stack: "React, Firebase, Javascript , Css", link: "https://chatapproj.netlify.app/" },
-  { id: 2, image: "/assets/project2.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
-  { id: 3, image: "/assets/project3.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
-  { id: 4, image: Project4, title: "PJL RealEstate", stack: "C#,SSMSQL,Entity Framework, CSS, JS,ASP.NET CORE,Docker and Kubernetes", link: "https://github.com/Delson-James17/RealnaRealEstate" },
-  { id: 5, image: "/assets/project5.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
-  { id: 6, image: "/assets/project6.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
+  { id: 2, image: Project2, title: "Valentines App", stack: "React, Javascript, Css , Email JS", link: "https://arawngmgapuso.netlify.app/" },
+  { id: 3, image: Project3, title: "PJL RealEstate", stack: "C#,SSMSQL,Entity Framework, CSS, JS,ASP.NET CORE,Docker and Kubernetes", link: "https://github.com/Delson-James17/RealnaRealEstate" },
+  // { id: 4, image: "/assets/project3.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
+  // { id: 5, image: "/assets/project5.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
+  // { id: 6, image: "/assets/project6.jpg", title: "Website Coding", stack: "HTML, CSS, JS", link: "#" },
 ];
 
 export default function Projects() {
